05ea8332f5dafa60ededab43c4ace8d8ebf671f8
Wincent Colaiuta <win@wincent.com>
2007-02-26 17:01:06 +0000

    Remove diff code from acceptance spec pending better implementation

--- spec/acceptance/acceptance_spec.rb
+++ spec/acceptance/acceptance_spec.rb
@@ -2,7 +2,6 @@
 # $Id$
 
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
-require 'walrus/parser' # ensure that WalrusGrammar is defined before continuing
 
 module Walrus
   
@@ -10,43 +9,35 @@ module Walrus
     
     # This spec performs high-level acceptance testing by running Walrus on the sample templates in the subdirectories of the "spec/acceptance/" directory and comparing them with the expected output.
     context 'processing test files with Walrus' do
-    
-      context_setup do
       
+      context_setup do
+        
         # construct an array of absolute paths indicating the location of all testable templates.
         @template_paths = Dir[File.join(File.dirname(__FILE__), '**/*.tmpl')].collect { |template| Pathname.new(template).realpath }
-      
+        
         # corresponding array of expected output files
         @expected_output_paths = @template_paths.collect { |path| path.to_s.sub(/\.tmpl\z/i, ".expected") }
-      
+        
         # class names based on template filename (less ".tmpl" extension)
         @class_names = @template_paths.collect { |path| Pathname.new(path).basename.to_s.gsub(/\.tmpl\z/i, '').to_class_name } 
-      
+        
         @parser = Parser.new
-      
+        
       end
-    
+      
       specify 'actual output should match expected output using "walrus" commandline tool' do
       
       
       end
-    
+      
       specify 'actual output should match expected output bypassing "walrus" commandline tool' do
         @template_paths.each_with_index do |template, index|
           actual_output           = self.class.class_eval(@parser.compile(IO.read(template), :class_name => @class_names[index]))
           expected_output         = IO.read(@expected_output_paths[index])
-          if actual_output != expected_output
-            printf actual_output
-            require 'walrus/diff'
-            puts "diff >>"
-            puts Diff.diff(actual_output, Pathname.new(@expected_output_paths[index])).inspect
-            puts "end"
-          end
           actual_output.should == expected_output
-        end
-      
+        end        
       end
-    
+      
     end
     
   end
