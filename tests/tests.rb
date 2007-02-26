943d757713743ddc1c11f6e2371613cf40b20d71
Wincent Colaiuta <win@wincent.com>
2007-02-26 11:05:46 +0000

    Remove tests made redundant by specs

--- tests/tests.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# Copyright 2007 Wincent Colaiuta
-# $Id$
-
-require 'test_helper'
-
-# Require (and run) anything named "*_test.rb" in "tests" and its subdirectories
-Dir[File.join(File.dirname(__FILE__), '**/*_test.rb')].each { |test| require test }
