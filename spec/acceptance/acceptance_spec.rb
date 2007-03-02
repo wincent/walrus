545dce123cbd23a5a607c1b136708b5b1aa62acd
Wincent Colaiuta <win@wincent.com>
2007-03-02 16:13:30 +0000

    Additional sweep through acceptance tests, this time running the compiled Ruby files in a subshell

--- spec/acceptance/acceptance_spec.rb
+++ spec/acceptance/acceptance_spec.rb
@@ -3,6 +3,8 @@
 
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 require 'walrus/parser' # ensure that WalrusGrammar is defined before continuing
+require 'mkdtemp'
+require 'open3'
 
 module Walrus
   
@@ -10,7 +12,6 @@ module Walrus
     
     # This spec performs high-level acceptance testing by running Walrus on the sample templates in the subdirectories of the "spec/acceptance/" directory and comparing them with the expected output.
     context 'processing test files with Walrus' do
-      
       
       # construct an array of absolute paths indicating the location of all testable templates.
       @template_paths = Dir[File.join(File.dirname(__FILE__), '**/*.tmpl')].collect { |template| Pathname.new(template).realpath }
@@ -21,19 +22,33 @@ module Walrus
       # class names based on template filename (less ".tmpl" extension)
       @class_names = @template_paths.collect { |path| Pathname.new(path).basename.to_s.gsub(/\.tmpl\z/i, '').to_class_name } 
       
+      # make temporary output dir for storing compiled templates
+      tmpdir = Pathname.new(Dir.mkdtemp('/tmp/walrus.acceptance.XXXXXX'))
+      
       @parser = Parser.new
       
-      specify 'actual output should match expected output using "walrus" commandline tool' do
-        
-      end
-      
       @template_paths.each_with_index do |template, index|
-        compiled        = @parser.compile(IO.read(template), :class_name => @class_names[index])
-        actual_output   = self.class.class_eval(compiled)
+        
+        class_name      = @class_names[index]
+        compiled        = @parser.compile(IO.read(template), :class_name => class_name)
         expected_output = IO.read(@expected_output_paths[index])
-        specify "actual output should match expected output bypassing 'walrus' commandline tool (source file: #{template})" do
+        
+        specify "actual output should match expected output evaluating dynamically (source file: #{template})" do
+          actual_output = self.class.class_eval(compiled)
           actual_output.should == expected_output
         end
+        
+        specify "actual output should match expected output running compiled file in subshell (source file: #{template})" do
+          target_path = tmpdir.join(class_name.to_require_name + '.rb')
+          File.open(target_path, 'w+') { |file| file.puts compiled }
+          actual_output = `ruby -I#{Walrus::SpecHelper::LIBDIR} -I#{Walrus::SpecHelper::EXTDIR} #{target_path}`
+          actual_output.should == expected_output
+        end
+        
+        specify "actual output should match expected output using 'walrus' commandline tool (source file: #{template})" do
+          
+        end
+        
       end
       
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,17 @@ require 'spec'
 module Walrus
   module SpecHelper
     
-    # will append the local libs directory to search path if not already present
-    parent      = Pathname.new(File.join(File.dirname(__FILE__), '..', 'lib')).realpath
-    extensions  = Pathname.new(File.join(parent, '..', 'ext')).realpath
-    
-    # normalize all paths ("rescue" because "realpath" will raise if lstat(2) fails for the path: non-existent paths, relative paths etc)
-    normalized = $:.collect { |path| Pathname.new(path).realpath rescue path }
-    
-    # only add the directories if they do not appear to be present already
-    [parent, extensions].each { |path| $:.push(path) unless normalized.include?(path) }    
+    if not const_defined? "LIBDIR"
+      # will append the local "lib" and "ext" directories to search path if not already present
+      LIBDIR  = Pathname.new(File.join(File.dirname(__FILE__), '..', 'lib')).realpath
+      EXTDIR  = Pathname.new(File.join(LIBDIR, '..', 'ext')).realpath
+      
+      # normalize all paths in the load path (use "rescue" because "realpath" will raise if lstat(2) fails for the path: non-existent paths, relative paths etc)
+      normalized = $:.collect { |path| Pathname.new(path).realpath rescue path }
+      
+      # only add the directories if they do not appear to be present already
+      [LIBDIR, EXTDIR].each { |path| $:.push(path) unless normalized.include?(path) }    
+    end
     
   end # module SpecHelper
 end # module Walrus
