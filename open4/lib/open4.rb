28271bfeb90488f2be389a1a84ef631f7d86ec51
Wincent Colaiuta <win@wincent.com>
2007-03-27 17:07:22 +0000

    Add open4

---
+++ open4/lib/open4.rb
@@ -0,0 +1,70 @@
+# Copyright 2007 Wincent Colaiuta
+# $Id$
+
+# Replacement for Open3
+# Unlike Open3, Open4 does not throw away the exit code of the executed (grandchild) process.
+# Only a child process is spawned and the exit status is returned in $? as normal.
+module Open4
+  
+  class Opener
+    
+    READ  = 0
+    WRITE = 1
+    
+    def initialize(*cmd)
+      
+      write_pipe  = IO::pipe
+      read_pipe   = IO::pipe
+      error_pipe  = IO::pipe
+      
+      pid = fork do
+        
+        # in child
+        write_pipe[WRITE].close
+        STDIN.reopen(write_pipe[READ])    # file descriptor duplicated here
+        write_pipe[READ].close            # must close in order for parent to see EOF
+        
+        read_pipe[READ].close
+        STDOUT.reopen(read_pipe[WRITE])
+        read_pipe[WRITE].close
+        
+        error_pipe[READ].close
+        STDERR.reopen(error_pipe[WRITE])
+        error_pipe[WRITE].close
+        
+        exec(*cmd)  # will raise SystemCallError if command can't be executed (eg Errno::NOENT)
+        exit!       # should never get here
+        
+      end
+      
+      # in parent
+      write_pipe[READ].close
+      read_pipe[WRITE].close
+      error_pipe[WRITE].close
+      
+      pipes = [write_pipe[WRITE], read_pipe[READ], error_pipe[READ]]
+      write_pipe[WRITE].sync = true
+      
+      if defined? yield
+        begin
+          result = yield(*pipes)
+          Process.waitpid(pid) # global $? gets set here
+          return result
+        ensure
+          pipes.each { |pipe| pipe.close unless pipe.closed? }
+        end
+      end
+      
+      # can't wait immediately (will hang); wait once garbage collection destroys/finalizes this instance
+      ObjectSpace.define_finalizer(self, proc { Process.waitpid(pid) })
+      pipes
+      
+    end
+    
+  end
+  
+  def self.popen4(*cmd, &block)
+    Opener.new(*cmd, &block)
+  end
+  
+end # module Open4
