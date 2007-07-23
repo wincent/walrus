273134611809fae6f8553258cd66d1dc21a5cb31
Wincent Colaiuta <win@wincent.com>
2007-07-23 12:14:43 +0200

    Update license to GPL 3
    Update included license file and license header in all source files to the GPL
    version 3.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- wopen3/lib/wopen3.rb
+++ wopen3/lib/wopen3.rb
@@ -1,4 +1,16 @@
 # Copyright 2007 Wincent Colaiuta
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # $Id: wopen3.rb 185 2007-04-12 16:56:29Z wincent $
 
 # Wopen3 is a replacement for Open3.
