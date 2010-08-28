af2a030777f2b2c80054fc224a29a57c33a9571b
Wincent Colaiuta <win@wincent.com>
2010-08-28 22:15:40 +0200

    Ruby 1.9: fix some broken specs
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/compiler_spec.rb
+++ spec/compiler_spec.rb
@@ -30,24 +30,24 @@ describe Walrus::Compiler do
   it 'compiles a comment followed by raw text' do
     # note that trailing newline is eaten when the comment is the only thing on
     # the line
-    eval @parser.compile "## hello world\nhere's some raw text",
+    Object.class_eval @parser.compile "## hello world\nhere's some raw text",
       :class_name => :CompilerSpecAlpha
     Walrus::Grammar::CompilerSpecAlpha.new.fill.should == "here's some raw text"
   end
 
   it 'compiles raw text followed by a comment' do
     # on the same line (note that trailing newline is not eaten)
-    eval @parser.compile "here's some raw text## hello world\n",
+    Object.class_eval @parser.compile "here's some raw text## hello world\n",
       :class_name => :CompilerSpecBeta
     Walrus::Grammar::CompilerSpecBeta.new.fill.should == "here's some raw text\n"
 
     # on two separate lines (note that second trailing newline gets eaten)
-    eval @parser.compile "here's some raw text\n## hello world\n",
+    Object.class_eval @parser.compile "here's some raw text\n## hello world\n",
       :class_name => :CompilerSpecDelta
     Walrus::Grammar::CompilerSpecDelta.new.fill.should == "here's some raw text\n"
 
     # same but with no trailing newline
-    eval @parser.compile "here's some raw text\n## hello world",
+    Object.class_eval @parser.compile "here's some raw text\n## hello world",
       :class_name => :CompilerSpecGamma
     Walrus::Grammar::CompilerSpecGamma.new.fill.should == "here's some raw text\n"
   end
