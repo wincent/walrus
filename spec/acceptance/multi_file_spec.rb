a4efb2f7a2ef870590c1225311b4bd56ab5f12f3
Wincent Colaiuta <win@wincent.com>
2010-08-29 02:08:25 +0200

    Split acceptance tests into subdirectories
    What was formerly all lumped together under:
    
      spec/acceptance
    
    Now is split into two subdirectories:
    
      spec/acceptance/single_file
      spec/acceptance/multi_file
    
    This is in preparation for the addition of a third type of acceptance
    test, which will take an existing template set (eg the Hextrapolate
    docs) and run Walrus over that, checking that the output is constant
    across versions of Ruby and Walrus.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/acceptance/multi_file_spec.rb
+++ spec/acceptance/multi_file_spec.rb
@@ -23,53 +23,6 @@
 require File.expand_path('../spec_helper', File.dirname(__FILE__))
 require 'mkdtemp'
 
-# This spec performs high-level acceptance testing by running Walrus on the
-# sample templates in the subdirectories of the "spec/acceptance/" directory
-# and comparing them with the expected output.
-describe 'processing test files with Walrus' do
-  # construct an array of absolute paths indicating the location of all
-  # testable templates.
-  template_paths = Dir[File.join(File.dirname(__FILE__), '**/*.tmpl')].map { |template| Pathname.new(template).realpath }
-
-  # make temporary output dirs for storing compiled templates
-  manually_compiled_templates = Pathname.new(Dir.mkdtemp('/tmp/walrus.acceptance.XXXXXX'))
-  walrus_compiled_templates   = Pathname.new(Dir.mkdtemp('/tmp/walrus.acceptance.XXXXXX'))
-  parser                      = Walrus::Parser.new
-  search_additions            = "#{ENV['RUBYLIB']}:#{Walrus::SpecHelper::LIBDIR}"
-
-  template_paths.each do |path|
-    template        = Walrus::Template.new(path)
-    compiled        = nil
-    it 'compiles (source file: #{path})' do
-      compiled      = template.compile
-    end
-    next if compiled.nil? # compiled will be nil if the compilation spec failed
-
-    expected_output = IO.read(path.to_s.sub(/\.tmpl\z/i, ".expected"))
-
-    it "matches expected output when evaluating dynamically (source file: #{path})" do
-      actual_output = template.fill
-      actual_output.should == expected_output
-    end
-
-    it "matches expected output when running compiled file in subshell (source file: #{path})" do
-      target_path = manually_compiled_templates.join(path.basename(path.extname).to_s + '.rb')
-      File.open(target_path, 'w+') { |file| file.puts compiled }
-      actual_output = `ruby -I#{Walrus::SpecHelper::LIBDIR} #{target_path}`
-      actual_output.should == expected_output
-    end
-
-    it "matches expected output when using 'walrus' commandline tool (source file: #{path})" do
-      `env RUBYLIB='#{search_additions}' #{Walrus::SpecHelper::TOOL} fill --output-dir '#{walrus_compiled_templates}' '#{path}'`
-      dir, base = path.split
-      dir   = dir.to_s.sub(/\A\//, '') if dir.absolute? # and always will be absolute
-      base  = base.basename(base.extname).to_s
-      actual_output = IO.read(walrus_compiled_templates + dir + base)
-      actual_output.should == expected_output
-    end
-  end
-end
-
 # These templates have a different extension to keep them separate from the other acceptance tests.
 describe 'processing multiple-interdependent files with Walrus' do
   template_paths  = Dir[File.join(File.dirname(__FILE__), '**/*.complex')].map { |template| Pathname.new(template).realpath }
