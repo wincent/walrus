93911fb39a389293ad9985c6701ab110448eafaa
Greg Hurrell <greg@hurrell.net>
2014-10-21 08:02:56 -0700

    Simplify remaining spec_helper requires
    For the same reason as the previous commit, `ag` didn't catch these
    files in its sweep.

--- spec/acceptance/complete_application_spec.rb
+++ spec/acceptance/complete_application_spec.rb
@@ -1,7 +1,7 @@
 # Copyright 2010-2014 Greg Hurrell. All rights reserved.
 # Licensed under the terms of the BSD 2-clause license.
 
-require File.expand_path('../spec_helper', File.dirname(__FILE__))
+require 'spec_helper'
 require 'mkdtemp'
 require 'wopen3'
 
--- spec/acceptance/single_file_spec.rb
+++ spec/acceptance/single_file_spec.rb
@@ -1,7 +1,7 @@
 # Copyright 2007-2014 Greg Hurrell. All rights reserved.
 # Licensed under the terms of the BSD 2-clause license.
 
-require File.expand_path('../spec_helper', File.dirname(__FILE__))
+require 'spec_helper'
 require 'mkdtemp'
 
 # This spec performs high-level acceptance testing by running Walrus on the
